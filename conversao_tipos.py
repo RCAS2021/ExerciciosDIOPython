7a0705d268790fb0e46ce6c04ebc6b51f1097cb9
Rafael Cruz <rcas2009@outlook.com>
2023-07-24 15:59:47 -0300

    conversao_tipos

---
+++ conversao_tipos.py
@@ -0,0 +1,24 @@
+print(int(1.6))
+print(type(int(1.6)))
+print(int("10"))
+print(type(int("10")))
+print(float("10.10"))
+print(type(float("10.10")))
+print(float(100))
+print(type(float(100)))
+print(str(10.10))
+print(type(str(10.10)))
+print(str(10))
+print(type(str(10)))
+
+valor = 10
+valor_str = str(valor)
+print(type(valor))
+print(type(valor_str))
+
+print(100/2)
+print(type(100/2))
+print(100//2)
+print(type(100//2))
+
+
